1c2d25d09a9ef6771d2d7aaff83fdbe5d0fcce39
CHOJANGHEUM <n_jero@naver.com>
2020-04-20 19:26:58 +0900

    씨발 세쎤 씨발

---
+++ kiito/src/main/java/global/sesoc/kiito/vo/Pfile.java
@@ -0,0 +1,14 @@
+package global.sesoc.kiito.vo;
+
+import lombok.Data;
+
+@Data
+public class Pfile {
+	
+	int pFile_seq;
+	int customer_seq;
+	String originalFilename;
+	String savedFilename;
+
+
+}
